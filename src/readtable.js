1ec472c971ef7bdfb061a0ca550e8eea7bd31715
Gabe Johnson <gijohnson105@gmail.com>
2017-02-24 14:38:02 -0600

    Use Table as Readtable backing store

--- src/readtable.js
+++ src/readtable.js
@@ -1,5 +1,7 @@
 // @flow
 
+import Table from './table';
+
 /*
  * 1. { key, mode: 'terminating', action } - creates a delimiter must return an Array/List
  * 2. { key, mode: 'non-terminating', action } - must return a Token or null/undefined. null/undefined simply consumes the read charstream.
@@ -27,38 +29,46 @@ type ReadtableMapping = {
   dispatchAction: Action
 };
 
+const entriesToPairs = (es) => es.map(({key, mode, action}) => [convertKey(key), { mode, action }]);
+
 export default class Readtable {
-  _entries: Array<ReadtableEntry>;
+  _entries: Table;
   constructor(entries: Array<ReadtableEntry> = []) {
-    this._entries = entries;
+    this._entries = Table.from(entriesToPairs(entries));
   }
 
   getMapping(key?: ReadtableKey): ReadtableMapping {
     if (!isValidKey(key)) throw Error('Invalid key type:', key);
     key = convertKey(key);
-    const { action, mode } = this._entries[key] || this._entries[0];
-    const dispatchEntry = this._entries[key + DISPATCH_OFFSET] || this._entries[DISPATCH_OFFSET] || {};
-    return { action, mode, dispatchAction: dispatchEntry.action };
+    // $FlowFixMe: deal w/ possible undefined
+    return this._entries.get(key);
   }
 
   extend(...entries: Array<ReadtableEntry>): Readtable {
-    const newTable = this._entries.slice();
-    return new Readtable(entries.reduce(addEntry, newTable));
+    entries.forEach(e => {
+      if (!isValidEntry(e)) throw Error('Invalid readtable entry:', e);
+    });
+    // const newTable = this._entries.slice();
+    // return new Readtable(entries.reduce(addEntry, newTable));
+    const newReadtable = new Readtable();
+    // $FlowFixMe: computed property keys not supported
+    newReadtable._entries = this._entries['fantasy-land/concat'](Table.from(entriesToPairs(entries)));
+    return newReadtable;
   }
 }
 
-function addEntry(table: Array<ReadtableEntry>, { key, mode, action }: ReadtableEntry): Array<ReadtableEntry> {
-  if (!isValidEntry({key, mode, action})) throw Error('Invalid readtable entry:', {key, mode, action});
+// function addEntry(table: Array<ReadtableEntry>, { key, mode, action }: ReadtableEntry): Array<ReadtableEntry> {
+//   if (!isValidEntry({key, mode, action})) throw Error('Invalid readtable entry:', {key, mode, action});
 
-  // null/undefined key is the default and will be converted to 0
-  // chars will be converted via codePointAt
-  // numbers are...numbers
-  // to accommodate default (null) 1 will be added to all and default will be at 0
-  // if is a dispatch macro, we have to convert the key and bump it up by DISPATCH_OFFSET
-  table[convertKey(key) + (mode === 'dispatch' ? DISPATCH_OFFSET : 0)] = { action, mode };
+//   // null/undefined key is the default and will be converted to 0
+//   // chars will be converted via codePointAt
+//   // numbers are...numbers
+//   // to accommodate default (null) 1 will be added to all and default will be at 0
+//   // if is a dispatch macro, we have to convert the key and bump it up by DISPATCH_OFFSET
+//   table[convertKey(key) + (mode === 'dispatch' ? DISPATCH_OFFSET : 0)] = { action, mode };
 
-  return table;
-}
+//   return table;
+// }
 
 export const EmptyReadtable = new Readtable([{
   mode: 'non-terminating',
@@ -85,6 +95,6 @@ function isValidEntry(entry: ReadtableEntry) {
   return entry && isValidKey(entry.key) && isValidMode(entry.mode) && isValidAction(entry.action);
 }
 
-function convertKey(key?: ReadtableKey): number {
-  return key == null ? 0 : (typeof key === 'number' ? key : key.codePointAt(0)) + 1;
+function convertKey(key?: ReadtableKey): string | null | void {
+  return typeof key === 'number' ? String.fromCodePoint(key) : key;
 }
